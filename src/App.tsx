9faf89b4dbe709c251258747a7eddf1cd71947f0
Barami Watthanakunanon <barami.wat@cpf.co.th>
2024-03-06 17:06:06 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
     <Router>
       <Switch>
         <Route path="/" component={() => <>HOME</>} />
-        <Route path="/login" component={() => <>LOGIN</>} />
+        <Route path="/login" component={() => <>LOGIN A</>} />
       </Switch>
     </Router>
   );
